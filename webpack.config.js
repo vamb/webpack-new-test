a97af3d8e78bdc228db75a64f1fbd6dbf16b544d
zhangsy <zhangsy@think-land.com>
2023-03-28 09:45:07 +0800

    feat: 优化css解析

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
+  entry: './src/index.js',
   devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: '模块标识符',
-    }),
-  ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
   module: {
     rules: [
       {
@@ -23,10 +15,35 @@ module.exports = {
           options: { presets: ['@babel/preset-typescript'] }
         }
       },
+      // {
+      //   test: /\.css$/i,
+      //   use: ["style-loader", "css-loader"],
+      // },
+      /**
+       * style-loader + css-loader 处理后，样式代码最终会被写入 Bundle 文件，并在运行时通过 style 标签注入到页面。这种将 JS、CSS 代码合并进同一个产物文件的方式有几个问题：
+       * 1. JS、CSS 资源无法并行加载，从而降低页面性能；
+       * 2. 资源缓存粒度变大，JS、CSS 任意一种变更都会致使缓存失效。
+       */
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+        test: /\.css$/,
+        use: [
+          // 根据运行环境判断使用那个 loader
+          (process.env.NODE_ENV === 'development' ?
+            'style-loader' :
+            MiniCssExtractPlugin.loader),
+          'css-loader'
+        ]
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      title: '模块标识符',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
   },
 }
